8a4dc8236a08dcd3eff8d332018d688f70f99b79
Mistry Dhruvil <dkmistry0203@gmail.com>
2024-08-31 23:29:48 +0530

    add github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
           <span className="text-green-500 items-center">OP/&gt;</span>
          
         </div>
-        <a href="https://github.com/dhruvil0203?tab=repositories" target="_blank">
+        <a href="https://github.com" target="_blank">
         <button className="text-white bg-green-500 my-4 mx-2 ring-white ring-1 rounded-full flex justify-center items-center">
           <img className="invert w-10 p-1 px-1" src="public/github.png" alt="github logo"/>
            <span className="font-bold px-3 ">GitHub</span>
